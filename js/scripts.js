bf6986af0d73daccd98a2f06d0772b8290fc75b7
Jonah Johansen <johansenjonah+git@gmail.com>
2021-01-12 14:48:36 -0800

    finish lesson 35 Co-Authored By:Jer Miller <Gkstaff@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
+/*
 $(document).ready(function() {
   $("button#green").click(function() {
+    $("body").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
+    $("body").removeClass();
     $("body").addClass("yellow-background");
   });
 
   $("button#red").click(function() {
+    $("body").removeClass();
     $("body").addClass("red-background");
   });
+});
+*/
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").css("background-color", "green");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").css("background-color", "yellow");
+  });
+
+$("button#red").click(function() {
+  $("body").css("background-color", "red");
+});
 });
